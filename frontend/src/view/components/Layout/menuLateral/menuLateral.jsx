8ec62cfd989ef3acbbc8fe9f1d041f376875602d
Mikael <mikaelc623@gmail.com>
2026-05-08 16:49:42 -0300

    feat: adicionado overflow no body quando menu aberto

--- frontend/src/view/components/Layout/menuLateral/menuLateral.jsx
+++ frontend/src/view/components/Layout/menuLateral/menuLateral.jsx
@@ -1,11 +1,24 @@
 import { createPortal } from "react-dom";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 import { ShoppingCart, CircleUser } from "lucide-react";
 import { useAuth } from "../../../../context/AuthContext.jsx";
 
 function MobileMenu({ open, setOpen, handleSignOut }) {
   const { user } = useAuth();
+
+  useEffect(() => {
+    if (open) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, [open]);
 
   return createPortal(
     <>
@@ -16,54 +29,62 @@ function MobileMenu({ open, setOpen, handleSignOut }) {
         />
       )}
 
-      {/* Sidebar */}
       <div
         className={`fixed top-0 right-0 h-screen w-55 bg-sky-950 text-white z-[9999] transform transition-all duration-300 ${
-          open ? "translate-x-0" : "translate-x-full"
-        }`}
-      >
-          <div className="flex flex-col p-6 space-y-6">
+          open ? "translate-x-0" : "translate-x-full"}`}>
+
+        <div className="flex flex-col p-6 space-y-6">
           <h2 className="text-lg font-semibold">Menu</h2>
 
           {user ? (
             <>
-            <div className="flex flex-col gap-4">
-              <Link 
-                to="/perfil" 
-                onClick={() => setOpen(false)}
-                className="flex items-center gap-2 cursor-pointer hover:text-white/70 transition">
-                <CircleUser size={20} strokeWidth={1.5} />
-                <span>Perfil</span>
+              <div className="flex flex-col gap-4">
+                <Link
+                  to="/perfil"
+                  onClick={() => setOpen(false)}
+                  className="flex items-center gap-2 cursor-pointer hover:text-white/70 transition"
+                >
+                  <CircleUser size={20} strokeWidth={1.5} />
+                  <span>Perfil</span>
+                </Link>
+
+                <div className="flex items-center gap-2 cursor-pointer hover:text-white/70 transition">
+                  <ShoppingCart size={20} />
+                  <span>Carrinho</span>
+                </div>
+              </div>
+
+              <div className="border-t border-white/20" />
+
+              <button
+                onClick={() => {
+                  handleSignOut();
+                  setOpen(false);
+                }}
+                className="bg-white text-black px-4 py-2 rounded-full text-sm font-medium hover:scale-105 transition"
+              >
+                Sair
+              </button>
+            </>
+          ) : (
+            <>
+              <Link to="/cadastro" className="text-center">
+                Criar conta
               </Link>
 
-              <div className="flex items-center gap-2 cursor-pointer hover:text-white/70 transition">
-                <ShoppingCart size={20} />
-                <span>Carrinho</span>
-              </div>
-            </div>
-
-            <div className="border-t border-white/20" />
-
-            <button
-                onClick={() => {
-                handleSignOut();
-                setOpen(false);
-                }}
-                className="bg-white text-black px-4 py-2 rounded-full text-sm font-medium hover:scale-105 transition">
-                Sair
-            </button>
-
+              <Link
+                to="/login"
+                className="bg-white text-center text-black px-4 py-2 rounded-full text-sm font-medium hover:scale-105 transition"
+              >
+                Entrar
+              </Link>
             </>
-            ) : (
-              <>
-                <Link to="/cadastro" className="text-center">Criar conta</Link>
-                <Link to="/login" className="bg-white text-center text-black px-4 py-2 rounded-full text-sm font-medium hover:scale-105 transition">Entrar</Link>
-              </>
-            )}
+          )}
+        </div>
       </div>
-    </div>
-  </>,
+    </>,
     document.body
   );
 }
+
 export default MobileMenu;
